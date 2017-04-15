b557acefb126424daef0c260a33812db8017bd6b
Jullyana Mattos Vycas <jmattosvycas@uwaterloo.ca>
2017-04-15 14:05:54 -0300

    First version for a template.

---
+++ template/C.cpp
@@ -0,0 +1,30 @@
+#include <algorithm>
+#include <cmath>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <map>
+#include <sstream>
+#include <string>
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+int main(){
+  int numTestCases, currTestCase;
+
+
+  currTestCase = 1;
+  cin >> numTestCases;
+  while(currTestCase <= numTestCases){
+
+
+    cout << "Case #" << currTestCase++ << ": ";
+    cout << endl;
+
+    // CLEAN UP
+  }
+
+  return 0;
+}
