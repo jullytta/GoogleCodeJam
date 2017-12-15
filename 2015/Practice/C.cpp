deb6561e35ffa4196aa379860c3964dc1dbfc345
Jullyana Mattos Vycas <jmattosvycas@uwaterloo.ca>
2017-12-15 15:41:04 -0300

    API naive simulator.

--- 2015/Practice/C.cpp
+++ 2015/Practice/C.cpp
@@ -1,16 +1,38 @@
 #include <iostream>
+#include <vector>
 #include <cilk/cilk.h>
 
 using namespace std;
 
-int main() {
+/* Simulating the API */
+/*   in a naive way   */
+int n_voters;
+vector<int> votes;
+
+void init(){
   int n_voters, vote;
   cin >> n_voters;
 
   for(int i = 0; i < n_voters; i++){
     cin >> vote;
-    cout << "Voter " << i+1 << " voted in " << vote << endl;
+    votes.push_back(vote);
   }
+}
+
+int GetN(){
+  return n_voters;
+}
+
+int GetVote(int i){
+  return votes[i];
+}
+
+int main() {
+
+  init();
+
+  cout << GetVote(1) << endl;
 
   return 0;
+
 }
