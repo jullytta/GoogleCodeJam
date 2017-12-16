07f260f578796c80e40c45cfd5282e2d2c37c2ce
Jullyana Mattos Vycas <jmattosvycas@uwaterloo.ca>
2017-12-16 14:47:53 -0300

    New way of finding a suspect: divide and conquer.

--- 2015/Practice/C.cpp
+++ 2015/Practice/C.cpp
@@ -9,41 +9,82 @@
 
 using namespace std;
 
-int main() {
+/* Finds a suspect of being the majority    */
+/* analyzing the votes between [begin, end] */
+/* Divide and conquer strategy              */
+/* Returns a pair <suspect, vote_count>     */
+/* If suspect == -1 no suspect was found    */
+pair<ll, ll> find_suspect(ll begin, ll end){
+  pair<ll, ll> suspect_1, suspect_2;
 
-  /* Find a suspect of being the majority */
-  ll current_suspect, number_of_votes = 0;
-  for(ll i = 0; i < GetN(); i++){
-    
-    if(number_of_votes == 0){
-      current_suspect = GetVote(i);
-      number_of_votes++;
+  /* More than 2 elements: partitions! */
+  if(end - begin > 1){
+    ll middle = (begin + end + 1)/2;
+
+    /* Avoiding the invalid use of ‘_Cilk_spawn’ error */
+    pair<ll, ll> spawn_suspect = cilk_spawn find_suspect(begin, middle);
+    suspect_2 = find_suspect(middle+1,end);
+    cilk_sync;
+    suspect_1 = spawn_suspect;
+  }
+  /* 1 or 2 elements */
+  else {
+    /* Only one element in this partition */
+    /* Therefore, it's the suspect!       */
+    if(begin == end){
+      return make_pair(GetVote(begin), 1);
+    }
+    /* Two elements: we might have a suspect */
+    /* if and only if the votes are equal    */
+    if(GetVote(begin) == GetVote(end)){
+      return make_pair(GetVote(begin), 2);
     }
     else {
-      if(current_suspect == GetVote(i)){
-        number_of_votes++;
-      }
-      else {
-        number_of_votes--;
-      }
+      /* No suspect found */
+      return make_pair(-1, 0);
     }
   }
 
-  /* Checks if the suspect actually won */
-  cilk::reducer< cilk::op_add<ll> > total_votes(0);
-  cilk_for(ll i = 0; i < GetN(); i++){
-    if(GetVote(i) == current_suspect){
-      *total_votes += 1;
-    }
-  }
+  /* Which of the two suspects found has more votes? */
+  /* If there is no suspect 1, return suspect 2 */
+  if(suspect_1.first == -1)
+    return suspect_2;
+  /* If there is a suspect 1, but they have less votes */
+  /* than suspect 2, subtract the votes and return 2   */
+  else if (suspect_1.second < suspect_2.second)
+    return make_pair(suspect_2.first, suspect_2.second - suspect_1.second);
+  /* Finally, if suspect 2 doesn't exist or have less  */
+  /* votes than 1, return 1 with the votes subtracted  */
+  else
+    return make_pair(suspect_1.first, suspect_1.second - suspect_2.second);
 
-  /* More than half of the votes = majority */
-  if(total_votes.get_value() > GetN()/2){
-    cout << current_suspect << endl;
-  }
-  else {
+}
+
+int main() {
+
+  pair<ll, ll> suspect = find_suspect(0, GetN()-1);
+
+  /* If we have no suspect, we have no winner! */
+  if(suspect.first == -1)
     cout << "NO WINNER" << endl;
+  else {
+    /* Checks if the suspect actually won */
+    cilk::reducer< cilk::op_add<ll> > total_votes(0);
+    cilk_for(ll i = 0; i < GetN(); i++){
+      if(GetVote(i) == suspect.first){
+        *total_votes += 1;
+      }
+    }
+
+    /* More than half of the votes = majority */
+    if(total_votes.get_value() > GetN()/2){
+      cout << suspect.first << endl;
+    }
+    else {
+      cout << "NO WINNER" << endl;
+    }
   }
+
 
   return 0;
 
